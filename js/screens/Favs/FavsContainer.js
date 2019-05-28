6600704ea0f48cc7338d1cca34f2d2a990834ca4
Naiji Zhang <naijizhang@gmail.com>
2019-05-28 14:14:59 -0700

    Add Favs Screen

---
+++ js/screens/Favs/FavsContainer.js
@@ -0,0 +1,13 @@
+//import liraries
+import React, { Component } from 'react';
+import Favs from "./Favs"
+// create a component
+class FavsContainer extends Component {
+    render() {
+        return (
+           <Favs/>
+        );
+    }
+}
+//make this component available to the app
+export default FavsContainer;
