18c64abd6b4bbc62bc553491133ec7156aa82870
Naiji Zhang <naijizhang@gmail.com>
2019-05-28 14:13:37 -0700

    Add Speaker Screen

---
+++ js/screens/Speaker/SpeakerContainer.js
@@ -0,0 +1,14 @@
+//import liraries
+import React, { Component } from 'react';
+import Speaker from "./Speaker"
+// create a component
+class SpeakerContainer extends Component {
+    render() {
+        return (
+           <Speaker/>
+        );
+    }
+}
+
+//make this component available to the app
+export default SpeakerContainer;
