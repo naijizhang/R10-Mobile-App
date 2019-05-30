888b283a22360e005f22f0918275d337d93886f9
Naiji Zhang <naijizhang@gmail.com>
2019-05-30 16:38:30 -0700

    Fix asyncStorage method bug

--- js/context/FavesContext/FavesContext.js
+++ js/context/FavesContext/FavesContext.js
@@ -1,64 +1,59 @@
 import React, { createContext, Component } from "react";
-import { queryFaves, createFav } from "../../config/models";
+import { queryFaves, createFave,removeFave } from "../../config/models";
 
 const FavesContext = React.createContext();
 class FavesProvider extends Component {
- constructor(props) {
-   super(props);
-   this.state = {
-     faveIds: []
-   };
- }
+  constructor(props) {
+    super(props);
+    this.state = {
+      faveIds: []
+    };
+  }
 
- async componentDidMount() {
-   this.getFavedSessionIds();
- }
+  async componentDidMount() {
+    this.getFavedSessionIds();
+  }
 
- getFavedSessionIds = async () => {
-   try {
-     const faves = await queryFaves();
-     const ids = faves.map(fave => fave[0]);
-     console.log(faves);
-     this.setState({ faveIds });
-   } catch (e) {
-     return false;
-   }
- };
-πaPP
- addFaveSession = async sessionId => {
-   try {
-     const newFave = await createFav(sessionId);
-     if (newFave) {
-       this.setState({ faveIds: [...this.state.faveIds, newFave.id] });
-     }
-   } catch (e) {
-     return false;
-   }
- };
+  getFavedSessionIds = async () => {
+    try {
+      const faves = await queryFaves();
+      const faveIds = faves.map(fave => fave[0]);
+      this.setState({ faveIds });
+    } catch (e) {
+      return false;
+    }
+  };
+  addFaveSession = async sessionId => {
+    try {
+      await createFave(sessionId);
+      this.getFavedSessionIds();
+    } catch (e) {
+      return false;
+    }
+  };
 
- removeFaveSession = async sessionId => {
-   try {
-     await removedFav(sessionId);
-     this.getFavedSessionIds();
-   } catch (e) {
-     return false;
-   }
- };
+  removeFaveSession = async sessionId => {
+    try {
+      await removeFave(sessionId);
+      this.getFavedSessionIds();
+    } catch (e) {
+      return false;
+    }
+  };
 
- render() {
-   console.log(this.state.faveIds);
-   return (
-     <FavesContext.Provider
-       value={{
-         ...this.state,
-         addFaveSession: this.addFaveSession,
-         removeFaveSession: this.removeFaveSession
-       }}
-     >
-       {this.props.children}
-     </FavesContext.Provider>
-   );
- }
+  render() {
+    return (
+      <FavesContext.Provider
+        value={{
+          ...this.state,
+          addFaveSession: this.addFaveSession,
+          removeFaveSession: this.removeFaveSession
+        }}
+      >
+        {this.props.children}
+      </FavesContext.Provider>
+    );
+  }
 }
 
 export { FavesProvider };
