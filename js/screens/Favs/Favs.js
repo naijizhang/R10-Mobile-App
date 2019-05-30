7361c9961a018b832fceea90770c69cd6927d532
Naiji Zhang <naijizhang@gmail.com>
2019-05-30 16:40:21 -0700

    Add context consumer for favs screen

--- js/screens/Favs/Favs.js
+++ js/screens/Favs/Favs.js
@@ -1,24 +1,25 @@
 //import liraries
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 // create a component
-const Favs = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Favs</Text>
-        </View>
-    );
+const Favs = ({ faveIds }) => {
+  console.log("favs on favscreen", faveIds);
+  return (
+    <View style={styles.container}>
+      <Text>Favs</Text>
+    </View>
+  );
 };
 
 // define your styles
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#ffffff',
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffff"
+  }
 });
 
 //make this component available to the app
--- js/screens/Favs/FavsContainer.js
+++ js/screens/Favs/FavsContainer.js
@@ -1,16 +1,19 @@
 //import liraries
-import React, { Component } from 'react';
-import Favs from "./Favs"
+import React, { Component } from "react";
+import Favs from "./Favs";
+import FavesContext from "../../context/FavesContext";
 // create a component
 class FavsContainer extends Component {
-    static navigationOptions = {
-        title: "Favs"
-      };
-    render() {
-        return (
-           <Favs/>
-        );
-    }
+  static navigationOptions = {
+    title: "Favs"
+  };
+  render() {
+    return (
+      <FavesContext.Consumer>
+        {value => <Favs faveIds={value.faveIds} />}
+      </FavesContext.Consumer>
+    );
+  }
 }
 //make this component available to the app
 export default FavsContainer;
